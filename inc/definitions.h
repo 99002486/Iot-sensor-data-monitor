62c553fe61ceb143bfe43e4266ceeea4448bcb08
Pallavi <pallavi@esper.io>
2021-03-15 00:01:00 +0530

    add functions

--- inc/definitions.h
+++ inc/definitions.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 #define NO_OF_ENTRIES 5
+#define THRESHOLD_TEMP 27
 typedef enum{GROUND=0,FIRST,SECOND,THIRD,FOURTH,FIFTH} FLOOR;
 
 typedef enum{OFF=0,ON} AC_STATE;
@@ -24,6 +25,10 @@ FLOOR which_floor(char* floor_data);
 
 int sensor_data_read(sensor_data_t* sensor_data_array, FILE* sensor_data_file);
 
+int room_ac_on_func(sensor_data_t* sensor_data_array,sensor_data_t* room_ac_array);
 
+int room_temp_ac_state_func(sensor_data_t* sensor_data_array,sensor_data_t* room_temp_ac_array, int threshold_temp);
+
+sensor_data_t* highest_power_room_func(sensor_data_t* sensor_data_array);
 
 #endif
--- main.c
+++ main.c
@@ -2,11 +2,26 @@
 
 int main(){
     sensor_data_t* sensor_data_array=NULL;
+    sensor_data_t* room_ac_on_array=NULL;
+    sensor_data_t* room_temp_ac_array=NULL;
+    int room_count,room_power;
     FILE* sensor_data_file;
     sensor_data_file=fopen("sensor.csv","r");
     if(!sensor_data_file)
         printf("File opening error");
+
     sensor_data_array=init_sensor_array();
     sensor_data_read(sensor_data_array,sensor_data_file);
+    
+    room_ac_on_array=init_sensor_array();
+    room_count=room_ac_on_func(sensor_data_array,room_ac_on_array);
+
+    room_temp_ac_array=init_sensor_array();
+    room_count=room_temp_ac_state_func(sensor_data_array,room_temp_ac_array,THRESHOLD_TEMP);
+
+    sensor_data_t* highest_power_room; 
+    highest_power_room=highest_power_room_func(sensor_data_array);
+    printf("high temp reg");
+    printf("high pc room: %d\n",highest_power_room->room_no);
     return 0;
 }
